1d27ef616d39da1b031e7c7c3b5091faa5581713
m-r-ebrahimi-dev <mrebrahimidev@gmail.com>
2023-01-10 22:50:36 +0330

    refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"github.com/m-r-ebrahimi-dev/golang-simple-web-app/pkg/config"
 	"github.com/m-r-ebrahimi-dev/golang-simple-web-app/pkg/handlers"
+	"github.com/m-r-ebrahimi-dev/golang-simple-web-app/pkg/render"
+	"log"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", handlers.HomePage)
-	http.HandleFunc("/about", handlers.About)
 
-	err := http.ListenAndServe(portNumber, nil)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("error creating template cache: ", err)
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.HomePage)
+	http.HandleFunc("/about", handlers.Repo.About)
+
+	err = http.ListenAndServe(portNumber, nil)
 	if err != nil {
 		return
 	}
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,16 +1,37 @@
 package handlers
 
 import (
+	"github.com/m-r-ebrahimi-dev/golang-simple-web-app/pkg/config"
 	"github.com/m-r-ebrahimi-dev/golang-simple-web-app/pkg/render"
 	"net/http"
 )
 
+// Repo the repository used by handlers
+var Repo *Repository
+
+// Repository the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for new handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // HomePage is about index page
-func HomePage(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) HomePage(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is intro page
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }
