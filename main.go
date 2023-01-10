99f0a6ccb9f1c6908be66af7bfb7108629acdd7a
m-r-ebrahimi-dev <mrebrahimidev@gmail.com>
2023-01-10 18:42:00 +0330

    start server on :8080 and get hello world response...

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
-package myapp
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello World")
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
+}
