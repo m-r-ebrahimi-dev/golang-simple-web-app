04de459455b43ef29c01805b9e9fc7b75d33fba3
m-r-ebrahimi-dev <mrebrahimidev@gmail.com>
2023-01-11 10:35:24 +0330

    add custom middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Use(middleware.Logger)
 	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
 
 	mux.Get("/", handlers.Repo.HomePage)
 	mux.Get("/about", handlers.Repo.About)
